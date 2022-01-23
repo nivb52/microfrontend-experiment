0d9d1a857dce837eb3271be5458afd9e4b9f35f0
Niv <nivb52@gmail.com>
2022-01-23 16:00:19 +0200

    header & footer

---
+++ home/src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="p-5 bg-blue-500 text-white text-xl font-bold">
+      only the best spinner 2022
+    </div>
+  );
+};
