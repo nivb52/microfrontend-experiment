b9174a73e12b9ccaf7558914ab979f163bf7b579
Niv <nivb52@gmail.com>
2022-01-29 22:09:16 +0200

    cart content page

--- cart/src/App.jsx
+++ cart/src/App.jsx
@@ -3,20 +3,22 @@ import ReactDOM from "react-dom";
 
 const Header = React.lazy(() => import("home/Header"));
 const Footer = React.lazy(() => import("home/Footer"));
+const CartContent = React.lazy(() => import("cart/CartContent"));
 
 import "remixicon/fonts/remixicon.css";
 import "./index.scss";
-import CartContent from "./CartContent";
 
 const App = () => (
   <div className="mx-auto max-w-6xl">
-    <Suspense fallback="<header class='mt-4'></header>">
+    <Suspense fallback={<header class="mt-4"></header>}>
       <Header />
     </Suspense>
 
-    <CartContent className="mt-4 mb-4" />
+    <Suspense fallback={<span>Loading ...</span>}>
+      <CartContent className="mt-4 mb-4" />
+    </Suspense>
 
-    <Suspense fallback="<footer class='mt-4'></footer>">
+    <Suspense fallback={<footer class="mt-4"></footer>}>
       <Footer />
     </Suspense>
   </div>
--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -1,20 +1,50 @@
 import React, { useState, useEffect } from "react";
-import Login from "./Login";
-import MiniCart from "./MiniCart";
 import { jwt } from "./services/login.service";
+import { cart } from "./services/cart.service";
+import { currency } from "home/products.service";
+
 
 export default function CartContent() {
-  const [token, setToken] = useState("");
+  const [cartItems, setCartItems] = useState([]);
+
 
   useEffect(() => {
-    return jwt.subscribe((val) => setToken(val ?? ""));
+    const unsubscribe = cart.subscribe((val) => {
+      setCartItems(val);
+    });
+
+    return () => {
+      unsubscribe();
+      setCartItems([]);
+    };
   }, []);
 
   return (
-    <div>
-      <div>JWT: {token}</div>
-      <Login />
-      <MiniCart />
-    </div>
+    <>
+      {jwt.value ? 
+      <div className="grid grid-cols-4 gap-5">
+        {Array.isArray(cartItems) &&
+          cartItems.map((item) => (
+            <React.Fragment key={item.id}>
+              <div>{item.quantity}</div>
+              <img src={item.image} alt={item.name} className="max-h-6" />
+              <div>{item.name}</div>
+              <div className="text-right">
+                {currency.format(item.quantity * item.price)}
+              </div>
+            </React.Fragment>
+          ))}
+        <div></div>
+        <div></div>
+        <div></div>
+
+        {currency.format(
+          Array.isArray(cartItems) &&
+            cartItems.reduce((a, c) => a + c.quantity * c.price, 0)
+        )}
+        </div>
+        : <div> Please login to see your cart</div>
+        }
+    </>
   );
 }
