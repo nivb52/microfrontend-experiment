b368529e5e907f610ce930657a916338d060f639
Niv <nivb52@gmail.com>
2022-01-30 02:37:51 +0200

    using cart in the header

--- home/src/HomeContent.jsx
+++ home/src/HomeContent.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getProducts, currency } from "./services/products";
+import { useLoggedIn } from "cart/hooks.useLogin";
+import { addToCart } from "cart/cart.service"
 export default function HomeContent() {
   const [products, setProducts] = useState([]);
 
@@ -7,6 +9,7 @@ export default function HomeContent() {
     getProducts().then(setProducts);
   }, []);
 
+  const loggedIn = useLoggedIn();
   return (
     <div className="grid grid-cols-4 gap-5">
       {products.map((product) => (
@@ -16,9 +19,18 @@ export default function HomeContent() {
             <div className="flex-grow text-sm font-medium">
               <a>{product.name}</a>
               <div className="flex-end text-xs">
-                          {currency.format(product.price)}
+                {currency.format(product.price)}
               </div>
-                <p className="mt-2">{product.description}</p>
+              <p className="mt-2">{product.description}</p>
+              {loggedIn && (
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={() => addToCart(product.id)}
+                  id={`addtocart_${product.id}`}
+                >
+                  Add to cart
+                </button>
+              )}
             </div>
           </div>
         </div>
