31459cce7527819a52fa73274a5768e6679574ee
Niv <nivb52@gmail.com>
2022-02-05 18:50:49 +0200

    addToCart mfe

---
+++ addtocart/src/App.jsx
@@ -0,0 +1,17 @@
+import { addToCart } from "./AddToCart";
+import { render } from "solid-js/web";
+
+import "./index.scss";
+
+const App = () => (
+  <>
+    <addToCart></addToCart>
+    <div class="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>Name: addtocart</div>
+      <div>Framework: solid-js</div>
+      <div>Language: JavaScript</div>
+      <div>CSS: Tailwind</div>
+    </div>
+  </>
+);
+render(App, document.getElementById("app"));
