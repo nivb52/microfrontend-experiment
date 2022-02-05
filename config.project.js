bcb9365147c135eab0b55b62a391a1a063a8a3bc
Niv <nivb52@gmail.com>
2022-02-05 18:52:26 +0200

    add note

--- config.project.js
+++ config.project.js
@@ -1,5 +1,6 @@
 module.exports = {
-  microeFrontends : {
+  microeFrontends: {
+    /**@note: if you change the port and want to use the ```npm run build:start``` you may also need to change the port in the package.json */
     home: {
       port: 8080,
     },
@@ -9,8 +10,11 @@ module.exports = {
     cart: {
       port: 8082,
     },
+    addtocart: {
+      port: 8083,
+    },
   },
   api_server: {
     port: 4000,
-  }
+  },
 };
